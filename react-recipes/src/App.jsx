b392966fd2a8cc2285ad25f0b7ef867b6417bb11
ivanmarcelomatos <102636454+ivanmarcelomatos@users.noreply.github.com>
2022-10-17 17:26:50 -0300

    first commit

---
+++ react-recipes/src/App.jsx
@@ -0,0 +1,143 @@
+import React from 'react';
+import ComponentTest from './ComponentTest';
+
+function App() {
+    return (
+        <article>
+            <h1>h1 teste 1352</h1>
+            <div>div teste</div>
+            <div>{ Date.now() }</div>
+            <ComponentTest />
+        </article>
+    )
+}
+
+export default App;
+//_____________________________________________
+// import React from 'react';
+// import ComponentTest from './ComponentTest';
+
+// function App() {
+//     return (
+//         <article>
+//             <h1>h1 teste 1350</h1>
+//             <div>div teste</div>
+//             <div>{ Date.now() }</div>
+//             <ComponentTest />
+//         </article>
+//     )
+// }
+
+// export default App;
+//_____________________________________________
+// import React from 'react';
+// import ComponentTest from './ComponentTest';
+
+// function App() {
+//     return(
+//         <article>
+//             <h1>h1 teste</h1>
+//             <div>div teste</div>
+//             <h3>{ Date.now() }</h3>
+//             <ComponentTest />
+//         </article>
+//     )
+// }
+
+// export default App;
+//_____________________________________________
+// import React from 'react';
+// import ComponentTest from './ComponentTest';
+
+// function App() {
+//     return (
+//         <article>
+//             <h1>h1 teste 1340</h1>
+//             <div>div teste</div>
+//             <div>{ Date.now() }</div>
+//             <ComponentTest />
+//         </article>
+//     )
+// }
+
+// export default App;
+//______________________________________________
+// import React from 'react';
+// import ComponentTest from './ComponentTest'
+
+// function App() {
+//     return (
+//         <article>
+//             <h1>h1 teste 1334</h1>
+//             <div>div teste</div>
+//             <h1>{ Date.now() }</h1>
+//             <ComponentTest />
+//         </article>
+//     )
+// }
+
+// export default App;
+//______________________________________________
+// import React from 'react';
+// import ComponentTest from './ComponentTest';
+
+// function App() {
+//     return (
+//         <article>
+//             <h1>h1 teste</h1>
+//             <div>div teste</div>
+//             <div>{ Date.now() }</div>
+//             <ComponentTest />
+//         </article>
+//     )
+// }
+// export default App;
+//______________________________________________
+// import React from 'react';
+// import ComponentTest from './ComponentTest';
+
+// function App() {
+//     return(
+//         <article>
+//             <h1>h1 teste 1236</h1>
+//             <div>div teste</div>
+//             <div>{ Date.now() }</div>
+//             <ComponentTest />
+//         </article>
+//     )
+// }
+
+// export default App;
+//______________________________________________
+// import React from 'react';
+// import ComponentTest from './ComponentTest';
+
+// function App() {
+//     return(
+//         <article>
+//             <h1>h1 Teste ivan</h1>
+//             <div>div teste 1230</div>
+//             <div>{ Date.now() }</div>
+//             <ComponentTest />
+//         </article>
+//     )
+// }
+
+// export default App;
+//______________________________________________
+// import React from 'react';
+// import ComponentTest from './ComponentTest';
+
+// function App() {
+//     return(
+//         <article>
+//             <h1>h1 teste</h1>
+//             <div>div teste</div>
+//             <div>{ Date.now() }</div>
+//             <ComponentTest />
+//         </article>
+//     )
+// }
+
+// export default App;
+//______________________________________________
---
+++ react-recipes/src/ComponentTest.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+
+function ComponentTest() {
+    const title = 'title em ComponentTest 2233'
+    return(
+        <div>{ title }</div>
+
+    )
+}
+
+export default ComponentTest;
+//_______________________________________
+// import React from 'react';
+
+// function ComponentTest() {
+//     const title = 'título em ComponentTest 2225';
+
+//     return (
+//         <div>{ title }</div>
+//     )
+// }
+
+// export default ComponentTest;
+//_______________________________________
+// import React from 'react';
+
+// function ComponentTest() {
+//     const title = 'Título no ComponentTest 2221';
+
+//     return (
+//         <div>{ title }</div>
+//     )
+// }
+
+// export default ComponentTest;
+//_______________________________________
+// import React from 'react';
+
+// function ComponentTest() {
+//     const title = 'Título no Componente 2212';
+
+//     return (
+//         <div>{ title }</div>
+//     )
+// }
+
+// export default ComponentTest;
+//_______________________________________
+// import React from 'react';
+
+// function ComponentTest() {
+//     const title = 'Título no ComponentTest 2203';
+//     return (
+//         <div> { title } </div>
+//     )
+// }
+
+// export default ComponentTest;
+//_______________________________________
+// import React from 'react';
+
+// function ComponentTest() {
+//     const title = 'título no ComponentTest';
+//     return (
+//         <div>{ title }</div>
+//     )
+// }
+
+// export default ComponentTest;
+//_______________________________________
+// import React from 'react';
+
+// function ComponentTest() {
+//     const title = 'title em ComponentTest 2149'
+//     return (
+//         <div>{ title }</div>
+//     )
+// }
+
+// export default ComponentTest;
+//_______________________________________
